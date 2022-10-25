e329c9f258fe59c8047e6821743542435cecd733
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-10-26 01:46:17 +0400

    feat: create register route

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('auth')->name('auth.')->group(function () {
+	Route::get('/login', function () {
+		return view('login');
+	})->name('login');
+
+	Route::get('/register', function () {
+		return view('register');
+	})->name('register');
 });
