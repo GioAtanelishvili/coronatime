c022611f27575940a6598ec98ee62c4704e08692
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-10-26 02:31:10 +0400

    style: create register form

--- resources/views/components/layouts/auth-layout.blade.php
+++ resources/views/components/layouts/auth-layout.blade.php
@@ -5,8 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Coronatime - {{ $title }}</title>
-        @vite("resources/css/app.css", "resources/js/helpers/Input.js", "resources/js/helpers/Validation.js")
-        @stack('scripts')
+        @vite('resources/css/app.css', 'resources/js/helpers/Input.js', 'resources/js/helpers/Validation.js')
+        
+        @if (request()->route()->named('auth.login'))
+            @vite('resources/js/login.js')
+        @elseif (request()->route()->named('auth.register'))
+            @vite('resources/js/register.js')
+        @endif
+
     </head>
     
     <body>
@@ -14,7 +20,7 @@
             <x-UI.logo />
         </header>
         
-        <main class="mt-10 md:mt-14 mx-4 md:mx-28">
+        <main class="my-10 md:my-14 mx-4 md:mx-28">
             <h1 class="text-black text-xl md:text-2xl font-black">{{ $heading }}</h1>
             <h2 class="text-zinc-500 mt-2 md:mt-4 text-base md:text-xl font-normal">{{ $subheading }}</h2>
             
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -4,30 +4,27 @@
     @endPush
 
     <x-slot:title>
-        Log In
+        Sign Up
     </x-slot:title>
 
     <x-slot:heading>
-        Welcome back
+        Welcome to Coronatime
     </x-slot:heading>
 
     <x-slot:subheading>
-        Welcome back! Please enter your details
+        Please enter required info to sign up
     </x-slot:subheading>
 
     <x-forms.auth-form action="/auth/login">
         <x-forms.input id="username" label="Username" name="username" placeholder="Enter unique username or email" />
+        <x-forms.input id="email" label="Email" name="email" placeholder="Enter your email" />
         <x-forms.input id="password" label="Password" name="password" placeholder="Fill in password" />
-
-        <div class="flex w-full md:w-1/2 md:min-w-[24rem] justify-between items-center">
-            <x-forms.checkbox />
-            <a href="#" class="text-blue-700 text-sm font-semibold">Forgot password?</a>
-        </div>
+        <x-forms.input id="repeat-password" label="Repeat password" name="repeatPassword" placeholder="Repeat password" />
         
-        <x-forms.submit>LOG IN</x-forms.submit>
+        <x-forms.submit>SIGN UP</x-forms.submit>
     </x-forms.auth-form>
 
     <x-slot:footer>
-        <p class="w-full md:w-1/2 md:min-w-[24rem]">Don't have an account? <a href="{{ route('auth.register') }}" class="text-black font-bold">Sign up for free</a></p>
+        <p class="w-full md:w-1/2 md:min-w-[24rem]">Already have an account? <a href="{{ route('auth.login') }}" class="text-black font-bold">Log in</a></p>
     </x-slot:footer>
 </x-layouts.auth-layout>
