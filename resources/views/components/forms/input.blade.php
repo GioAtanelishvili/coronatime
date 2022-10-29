a5d3048a69c74508e766077e3df6ef9da061110f
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-10-29 22:41:48 +0400

    feat: update form components to handle server side errors

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,12 +1,26 @@
 @props(['id', 'label', 'name', 'type' => 'text', 'placeholder'])
 
+@php
+    $isValid = old($name) && !$errors->first($name);
+    $isPassword = $type === 'password'
+@endphp
+
 <div class="flex w-full md:w-1/2 md:min-w-[24rem] relative flex-col gap-2">
     <label for="{{ $id }}" class="text-black text-sm md:text-base font-bold">{{ $label }}</label>
     <input 
         type="{{ $type }}" 
         name="{{ $name }}" 
         id="{{ $id }}" 
+        value="@if(!$isPassword){{ old($name) }}@endif"
         placeholder="{{ $placeholder }}" 
-        class="text-black placeholder:text-zinc-500 py-4.5 px-6 text-base font-normal border border-neutral-200 focus:border-blue-700 rounded-lg focus:shadow-input outline-none"
+        @class(['text-black placeholder:text-zinc-500 py-4.5 px-6 text-base font-normal border focus:border-blue-700 rounded-lg focus:shadow-input outline-none', 'border-neutral-200', 'border-green-600' => $isValid && !$isPassword])
     >
+
+    @if ($isValid && !$isPassword)
+        <x-forms.success :$name />
+    @endif
+
+    @error($name)
+        <x-forms.error :$name :$message/>
+    @enderror
 </div>
