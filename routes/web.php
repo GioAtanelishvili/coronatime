bda13e2430e6f7679b67b013b7a12de603c24b54
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-10-31 04:14:53 +0400

    feat: create route for login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\VerifyEmailController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('auth')->name('auth.')->group(function () {
 	Route::view('/login', 'login')->name('login');
+
+	Route::post('/login', LoginController::class)->name('login');
 
 	Route::view('/register', 'register')->name('register');
 
