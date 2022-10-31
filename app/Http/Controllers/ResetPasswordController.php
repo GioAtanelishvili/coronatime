faab0855042cffc4ad95f7bec46752c8036c80bf
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-11-01 03:58:17 +0400

    feat: create reset password controller

---
+++ app/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ResetPasswordRequest;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+	public function email(Request $request)
+	{
+		$request->validate(['email' => ['required', 'email', 'max:255']]);
+
+		$status = Password::sendResetLink($request->only('email'));
+
+		return $status === Password::RESET_LINK_SENT
+				? redirect()->route('password.notice')
+				: back()->withErrors(['email' => __($status)]);
+	}
+
+	public function show(Request $request, $token)
+	{
+		return view('reset-password', ['token' => $token, 'email' => $request->query('email')]);
+	}
+
+	public function update(ResetPasswordRequest $request)
+	{
+		Password::reset(
+			$request->validated(),
+			function ($user, $password) {
+				$user->forceFill([
+					'password' => $password,
+				]);
+
+				$user->save();
+
+				event(new PasswordReset($user));
+			}
+		);
+
+		return redirect()->route('auth.login');
+	}
+}
