41fb859e1be7fc67c35002229a168661d96145dc
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-11-01 20:43:04 +0400

    style: create reset password notification view

--- resources/views/email-notification.blade.php
+++ resources/views/email-notification.blade.php
@@ -3,9 +3,9 @@
         Verify email
     </x-slot:title>
     
-    <main class="flex mt-48 md:mt-80 flex-col justify-center items-center gap-4">
+    <main class="flex mt-48 md:mt-80 px-4 flex-col justify-center items-center gap-4">
         <x-UI.verification-notice-checkmark />
-        <p class="text-lg">
+        <p class="text-lg text-center">
             We have sent you a confirmation email
         </p>
     </main>
