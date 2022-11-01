3ff06fb2d325eb45a6484ba4a7f4fcc54fec4be4
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-11-01 07:12:57 +0400

    feat: send custom notification view when resetting password

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Auth\Notifications\VerifyEmail;
@@ -30,7 +32,23 @@ class AuthServiceProvider extends ServiceProvider
 		VerifyEmail::toMailUsing(function ($notifiable, $url) {
 			return (new MailMessage)
 				->subject('Verify Email Address')
-				->view('emails.verification', ['url' => $url]);
+				->view('emails.notification', [
+					'url'        => $url,
+					'heading'    => 'Confirmation email',
+					'subheading' => 'click this button to verify your email',
+					'button'     => 'VERIFY EMAIL',
+				]);
+		});
+
+		ResetPassword::toMailUsing(function ($notifiable, $token) {
+			return (new MailMessage)
+				->subject('Reset Password')
+				->view('emails.notification', [
+					'url'        => config('app.url') . '/reset-password/' . $token . '?email=' . $notifiable->email,
+					'heading'    => 'Recover password',
+					'subheading' => 'click this button to recover a password',
+					'button'     => 'RECOVER PASSWORD',
+				]);
 		});
 	}
 }
