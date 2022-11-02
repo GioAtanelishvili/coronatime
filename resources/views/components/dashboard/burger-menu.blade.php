76a381eb903ab1bc80471f5af58c037e446d311b
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-11-03 01:37:03 +0400

    style: create burger menu

---
+++ resources/views/components/dashboard/burger-menu.blade.php
@@ -0,0 +1,17 @@
+<div>
+    <button id="burger-menu-btn" class="peer block md:hidden">
+        <x-UI.burger-icon />
+    </button>
+
+    <div 
+        id="burger-menu" 
+        class="bg-zinc-100 flex mt-3 px-5 py-3 flex-col items-end gap-2 fixed left-full translate-x-0 peer-focus:-translate-x-full transition-transform ease-out duration-300 border-y border-l border-zinc-300 rounded-l-xl"
+    >
+        <span class="text-base font-bold capitalize">{{ auth()->user()->name }}</span>
+        <div class="bg-zinc-300 w-full h-[1px]"></div>
+        <form action="{{ route('auth.logout') }}" method="post">
+            @csrf()
+            <button type="submit">Log Out</button>
+        </form>
+    </div>
+</div>
--- resources/views/components/layouts/dashboard-layout.blade.php
+++ resources/views/components/layouts/dashboard-layout.blade.php
@@ -13,19 +13,19 @@
         <header class="flex mt-6 md:mt-5 ml-4 md:ml-28 mr-8 md:mr-28 justify-between items-center">
             <x-UI.logo />
 
-            <div>
-                <div class="hidden md:flex items-center gap-4">
-                    <span class="text-base font-bold capitalize">{{ auth()->user()->name }}</span>
-                    <div class="bg-neutral-200 w-[1px] h-8"></div>
-                    <form action="{{ route('auth.logout') }}" method="post">
-                        @csrf()
-                        <button type="submit">Log Out</button>
-                    </form>
-                </div>
+            <x-dashboard.burger-menu />
+
+            <div class="hidden md:flex items-center gap-4">
+                <span class="text-base font-bold capitalize">{{ auth()->user()->name }}</span>
+                <div class="bg-neutral-200 w-[1px] h-8"></div>
+                <form action="{{ route('auth.logout') }}" method="post">
+                    @csrf()
+                    <button type="submit">Log Out</button>
+                </form>
             </div>
         </header>
         
-        <main class="my-11 md:my-14 mx-4 md:mx-28">
+        <main class="mt-11 mb-5 md:my-14 mx-4 md:mx-28">
             <h1 class="text-black text-xl md:text-2xl font-black">Worldwide Statistics</h1>
             
             <x-dashboard.nav />
