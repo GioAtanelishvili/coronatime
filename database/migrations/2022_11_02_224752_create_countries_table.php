e7beda94c044ffaa91065298ef3a5fda8bca0821
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-11-03 05:57:41 +0400

    feat: create countries table migration

---
+++ database/migrations/2022_11_02_224752_create_countries_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('countries', function (Blueprint $table) {
+			$table->id();
+			$table->string('code')->unique();
+			$table->json('name');
+			$table->mediumInteger('confirmed');
+			$table->mediumInteger('recovered');
+			$table->mediumInteger('critical');
+			$table->mediumInteger('deaths');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('countries');
+	}
+};
