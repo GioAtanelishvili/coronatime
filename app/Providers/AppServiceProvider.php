0810caa0941f348a3546eb92c8d2f4ba35f6ccf1
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-11-03 05:58:26 +0400

    feat: create country seeder

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Http;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,5 +23,8 @@ class AppServiceProvider extends ServiceProvider
 	 */
 	public function boot()
 	{
+		Http::macro('devtest', function () {
+			return Http::baseUrl('https://devtest.ge');
+		});
 	}
 }
