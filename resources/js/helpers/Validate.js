b0fb690b0927702c0fc0f2abec56816135af4a98
GioAtanelishvili <gatanelishvili03@gmail.com>
2022-11-20 22:49:05 +0400

    fix: remove redundant language property from validate class

--- resources/js/helpers/Validate.js
+++ resources/js/helpers/Validate.js
@@ -4,8 +4,6 @@ const EMAIL_REGEX =
     /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 class Validate {
-    static language = document.getElementById("select").dataset.language;
-
     static errors = new Map();
 
     static name = (input) => {
